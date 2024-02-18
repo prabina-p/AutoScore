08a30792e53398a6dd8c80c055356503810ac8a1
tharvipop <apkulkarni@ucsd.edu>
2024-02-18 03:20:56 -0800

    gpt api to autosource.py

--- src/autoscore.py
+++ src/autoscore.py
@@ -2,12 +2,18 @@ import chromadb
 import pandas as pd
 import numpy as np
 import sklearn
+from openai import OpenAI
 from sklearn.feature_extraction.text import CountVectorizer
 import matplotlib.pyplot as plt
 
 
-### chroma db implementation:
 
+### thresholding implementation:
+
+
+
+
+### chroma db implementation:
 def create_client_collection():
     ''' 
     Create a client and collection using ChromaDB
@@ -83,6 +89,86 @@ def predict(response_json):
     correct_sum = [x['correct'] for x in response_json['metadatas'][0]].count('True')
     weighted_vote = (correct_sum/3) >= 0.5
     return weighted_vote
+
+
+### chat gpt implementation:
+API_KEY = "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"
+client = OpenAI(api_key=API_KEY)
+
+
+# True or False
+def bot_compare(question: str, solution: str, student_answer: str, special_requirements: str = None, strictness: int = 7) -> bool:
+	"""
+	Compare the student's answer with the standard answer under the context provided by the question.
+	:param question: The question.
+	:param solution: The standard answer.
+	:param student_answer: The student's answer.
+	:param special_requirements: Special requirements about the grading from the instructor.
+	:param strictness: A strictness scale out of 10.
+	:return: True if the student's answer is equivalent to the standard answer, False otherwise.
+	"""
+	prompt_1 = f"""Your role is a grader for free response question of a class, 
+					and the only thing you do is to do is to compare the student 
+					answer with a given standard answer under the context provided by the question. Since
+					 you are the grader, there could be some special requirements about the grading
+					from the instructor that you must follow when doing your grading.  You only output 1 or 0, 
+					according to whether they are equivalent or not under a strictness scale out of 10. You will 
+					be given the student's answer as the user's input. \n 
+					- The question is {question}. \n 
+					- The standard answer is {solution}. \n 
+					- Strictness: {strictness} \n
+					- Special Requirement: {special_requirements}"""
+
+	response = client.chat.completions.create(
+		model="gpt-4",
+		messages=[
+		{
+			"role": "system",
+			"content": prompt_1
+		},
+		{
+			"role": "user",
+			"content": student_answer
+		}
+		],
+		temperature=0.3,
+		max_tokens=64,
+		top_p=1
+	)
+	actual_response = response.choices[0].message.content
+	# print(f"Response: {bool(actual_response)}")
+	return bool(int(actual_response))
+
+def bot_suggests(question: str, solution: str, student_answer: str, special_requirements: str = None) -> str:
+	"""If the student's answer is not equivalent to the standard answer, the bot will suggest the student to improve his/her answer.
+	"""
+
+	prompt = f"""A student's free response answer is wrong for a question. You want to provide suggestions for student to improve 
+				his/her answer. The suggestions should guide his thinking process. 
+				You will be given the question prompt, solution, and student's answer as the user's input. Instructors 
+				might have some special instructions about the question. \n
+				- The question is {question}. \n
+				- The standard answer is {solution}. \n
+				- The student's answer is {student_answer}. \n
+				- Special Requirement: {special_requirements}"""
+	# Create chat gpt session and send the request
+	response = client.chat.completions.create(
+		model="gpt-4",
+		messages=[
+		{
+			"role": "system",
+			"content": prompt
+		}
+		],
+		temperature=0.3,
+		max_tokens=128,
+		top_p=1
+	)
+	actual_response = response.choices[0].message.content
+	return actual_response
+	
+
+
 
     
     
